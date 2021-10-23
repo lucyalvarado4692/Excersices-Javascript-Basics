3f2f07954f7a60cac7350abdc6b7ca86b61dbc49
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-10-23 10:59:05 +0200

    added file comparisonExercises and comparisonTest

---
+++ src/comparisonExercises.js
@@ -0,0 +1,6 @@
+export function isJane()
+{
+    /* La function debe devolver Jane, obligatorio el uso del comparador == */
+    let name = 'Jane';
+
+}
---
+++ tests/comparisonExercises.test.js
@@ -0,0 +1,9 @@
+import { isJane } from "../src/comparisonExercises";
+
+describe('Comparison Exercises', () => {
+    test('should be equal to Jane', () => {
+        /* Completa el test y modifica la function isJane */
+
+        expect(result).toEqual('Jane');
+    });
+});
