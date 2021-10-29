6d3015f12cd9e057fe50d8ac642bfdfa0522036f
pilpod <giacomo.corso@gmail.com>
2021-10-29 19:36:17 +0200

    opposite attract solved

--- tests/micro-ex/oppositesAttract.test.js
+++ tests/micro-ex/oppositesAttract.test.js
@@ -1,5 +1,6 @@
+import { isLove } from '../../src/micro-ex/oppositesAttract';
 
-describe('Basic tests', () => {
+describe('Opposites Attract', () => {
     test('Testing if love is in the air', () => {
         expect(isLove(1,4)).toEqual(true);
         expect(isLove(2,2)).toEqual(false);
