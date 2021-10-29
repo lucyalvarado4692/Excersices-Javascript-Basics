aa197a212a2515ea11ddff6b4c91e9ef50bd0bbd
pilpod <giacomo.corso@gmail.com>
2021-10-29 19:25:48 +0200

    added opposites attrack exercise

---
+++ tests/micro-ex/oppositesAttract.test.js
@@ -0,0 +1,9 @@
+
+describe('Basic tests', () => {
+    test('Testing if love is in the air', () => {
+        expect(isLove(1,4)).toEqual(true);
+        expect(isLove(2,2)).toEqual(false);
+        expect(isLove(0,1)).toEqual(true);
+        expect(isLove(0,0)).toEqual(false);
+    });
+});
