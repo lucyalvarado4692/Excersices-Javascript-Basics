80939d3949b86cac6cd1c575b85f2c7a94566bc9
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-10-30 15:20:14 +0200

    third angle triangle exercise tested

--- src/micro-ex/thirdAngleTriangle.js
+++ src/micro-ex/thirdAngleTriangle.js
@@ -7,3 +7,9 @@ Escribe una función que devuelva el tercer angulo.
 
 PD: Solo se realizarán los tests con números enteros
 */
+
+export function returnThirdAngle(angle1, angle2)
+{
+    let thirdAngleDegree = 180 - (angle1 + angle2);
+    return thirdAngleDegree;
+}
