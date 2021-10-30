688737002378edeabe599dd8f2f43d1dc62a3abb
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-10-30 15:08:08 +0200

    added third angle triangle exercise

---
+++ src/micro-ex/thirdAngleTriangle.js
@@ -0,0 +1,9 @@
+/* 
+
+## Third Angle of the Triangle
+
+Dado dos angulos interiores de un triangulo en grados.
+Escribe una función que devuelva el tercer angulo.
+
+PD: Solo se realizarán los tests con números enteros
+*/
