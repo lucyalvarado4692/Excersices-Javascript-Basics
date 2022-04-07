9fcb933ed4594d24c3f66d29fd6e18b505b23dc3
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2022-04-07 14:04:00 +0200

    comparisonExercises donde

--- src/fundamentals-ex/comparisonExercises.js
+++ src/fundamentals-ex/comparisonExercises.js
@@ -1,23 +1,22 @@
 export function isJane()
 {
     /* La function debe devolver Jane, obligatorio el uso del comparador == */
-    let name = 'Jane';
+    
 
 }
 
 export function compareNumbers()
 {
     /* La function debe devolver 10, obligatorio el uso del comparador === */
-    let number1 = '10';
-    let number2 = '10';
+    
 }
 
-export function isGreaterThan10(quantity)
+export function isGreaterThan10()
 {
     
 }
 
-export function isGreaterThan10AndEqualTo20(number)
+export function isGreaterThan10AndEqualTo20()
 {
     
 }
