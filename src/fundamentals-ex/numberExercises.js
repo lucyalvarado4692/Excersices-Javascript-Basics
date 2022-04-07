6bd4dac976b77815b20bbfc1e48ffd3dbe38a748
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2022-04-07 12:44:35 +0200

    numberExercices done

--- src/fundamentals-ex/numberExercises.js
+++ src/fundamentals-ex/numberExercises.js
@@ -1,18 +1,16 @@
-export function checkVariable()
+
+export function checkVariable(number)
 {
-    let number = 15;
     
 }
 
 export function checkIfIsInteger()
 {
-    const number = 303.33;
     
 }
 
 export function checkIfNumberIsInfinite()
 {
-    const number = 5 * Infinity ;
     /* Tendrás que utilizar un condicional / if ... */
     
 }
@@ -20,33 +18,31 @@ export function checkIfNumberIsInfinite()
 export function sum()
 {
     /* Completa la function utilizando el operador de suma + */
-    let numbOne = 20;
+
 }
 
 export function subtract()
 {
     /* Completa la function utilizando el operador de sustracción - */
-    const numbOne = 55;
     
 }
 
 export function multiply()
 {
     /* Completa la function utilizando el operador de sustracción - */
-    const numbOne = 3;
+    
 
 }
 
 export function divide()
 {
     /* Completa la function utilizando el operador de división / */
-    const numbOne = 50;
 
 }
 
 export function checkIfNumbIsDivisibleByThree()
 {
     /* Completa la function utilizando el operador modulus y utiliza el conditional if() */
-    const numbOne = 3;
+    
     
 }
